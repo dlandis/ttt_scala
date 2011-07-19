ce441ee5f65dc6674cf0dfaff7f36f142eaee98c
Mike Jansen <mjansen401@gmail.com>
2011-07-19 13:17:30 -0500

    add status and winner methods to board

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -1,5 +1,76 @@
 package com.tictactoe
 
 class Board {
-    def status() = { "Game In Progress" }
+    val InProgressStatus = "Game In Progress"
+    val DrawStatus = "Draw"
+    val EmptySquare = "-"
+    
+    private[this] var boardValues = new Array[String](9)
+    for (i <- 0 to 8) { boardValues(i) = EmptySquare }
+    
+    private[this] var boardStatus = InProgressStatus
+    
+    def status: String = boardStatus
+    
+    def getSquareValue(square:Int) = { 
+        boardValues(square)
+        }
+    
+    def setSquareValue(square:Int, symbol:String) = {
+        boardValues(square) = symbol
+    }
+    
+    def allSquaresOccupied = {
+        var moves = 0
+        for (i <- 0 to 8) {
+            if (boardValues(i) != "-") {
+                moves += 1
+            }
+        }
+        (moves == 9)
+        
+    }
+    
+    def updateStatus() = {
+        if ( isWinner("O") ) {
+            boardStatus = "O wins!"
+        }
+        else if ( isWinner("X") ) {
+            boardStatus = "X wins!"
+        }
+        else if (allSquaresOccupied) {
+            boardStatus = DrawStatus
+        }
+    }
+    
+    def isWinner(symbol:String) = {
+        ( rowsVictory(symbol) || columnsVictory(symbol) || diagonalsVictory(symbol) )
+    }
+    
+    def rowsVictory(symbol:String) = {
+        (
+            threeInARow(symbol, 0, 1, 2) || 
+            threeInARow(symbol, 3, 4, 5) || 
+            threeInARow(symbol, 6, 7, 8)
+        )
+    }
+    
+    def columnsVictory(symbol:String) = {
+        (
+            threeInARow(symbol, 0, 3, 6) || 
+            threeInARow(symbol, 1, 4, 7) || 
+            threeInARow(symbol, 2, 5, 8)
+        )
+    }
+    
+    def diagonalsVictory(symbol:String) = {
+        (
+            threeInARow(symbol, 0, 4, 8) || 
+            threeInARow(symbol, 2, 4, 6)
+        )
+    }
+    
+    def threeInARow(symbol:String, first:Int, second:Int, third:Int) = {
+        (boardValues(first) == symbol  && boardValues(second) == symbol && boardValues(third) == symbol)
+    }
 }
--- src/test/scala/BoardSpec.scala
+++ src/test/scala/BoardSpec.scala
@@ -3,9 +3,89 @@ import org.scalatest.Spec
 import com.tictactoe.Board
 
 class BoardSpec extends Spec {
-    describe("Status") {
-        it("has a default status of 'Game In Progress'") {
-            expect("Game In Progress") { new Board().status() }
+
+    def createBoard = new Board()
+    
+    def draw(board:Board) = {
+        board.setSquareValue(0, "O")
+        board.setSquareValue(1, "O")
+        board.setSquareValue(2, "X")
+        board.setSquareValue(3, "X")
+        board.setSquareValue(4, "X")
+        board.setSquareValue(5, "O")
+        board.setSquareValue(6, "O")
+        board.setSquareValue(7, "X")
+        board.setSquareValue(8, "X")
+    }
+    
+    def setWinner(board:Board, symbol:String) = {
+        board.setSquareValue(0, symbol)
+        board.setSquareValue(1, symbol)
+        board.setSquareValue(2, symbol)
+    }
+    
+    describe("square values") {
+        it("gets an empty square") {
+            var board = createBoard
+            expect("-") { board.getSquareValue(0)}
+        }
+        
+        it("sets a square") {
+            var board = createBoard
+            board.setSquareValue(0, "X")
+            
+            expect("X") { board.getSquareValue(0)}
+        }
+        
+        it("returns true if all squares occupied") {
+            var board = createBoard
+            
+            draw(board)
+            
+            expect(true) { board.allSquaresOccupied }
+        }
+        
+        it("returns false if not all squares occupied") {
+            var board = createBoard
+            
+            expect(false) { board.allSquaresOccupied }
         }
     }
+    
+    describe("Status") {
+        it("has a default status of 'Game In Progress'") {
+            var board = createBoard
+            expect("Game In Progress") { board.status }
+        }
+        
+        it("X wins based on board values") {
+            var board = createBoard
+            setWinner(board, "X")
+            
+            board.updateStatus()
+            
+            expect("X wins!") { board.status }
+        }
+        
+        it("O wins based on board values") {
+            var board = createBoard
+            setWinner(board, "O")
+            
+            board.updateStatus()
+            
+            expect("O wins!") { board.status }
+        }
+        
+        it("finds draw if all squares are occupied with no winner") {
+            var board = createBoard
+            draw(board)
+            
+            board.updateStatus()
+            
+            expect("Draw") { board.status }
+            
+        } 
+        
+    }
+    
 }

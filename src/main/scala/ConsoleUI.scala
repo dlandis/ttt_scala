72cbc00c5855e13c77a5bc9bb1581779e37b1bbb
Mike Jansen <mjansen401@gmail.com>
2011-07-21 11:04:13 -0500

    add methods to console UI

--- src/main/scala/ConsoleUI.scala
+++ src/main/scala/ConsoleUI.scala
@@ -1,15 +1,40 @@
 package com.tictactoe
 
-class ConsoleUI {
+class ConsoleUI(game: Game) {
     var io = Console
     var out = io.out
     var in = io.in
 
-    def welcomeUser {
-        println("Welcome to Tic Tac Toe in Scala")
+    def displayMessage(string: String) {
+        println(string)
     }
     
-
+    def displayBoard {
+        println("1    |2    |3    ")
+        printRow(0, 1, 2)
+        println("     |     |     ")
+        println("-----------------")
+        println("4    |5    |6    ")
+        printRow(3, 4, 5)
+        println("     |     |     ")
+        println("-----------------")
+        println("7    |8    |9    ")
+        printRow(6, 7, 8)
+        println("     |     |     ")
+        println(" ")
+    }
+    
+    def printRow(first: Int, second: Int, third: Int) {
+        println(
+            " " + game.getSquareValue(first) + 
+            " " + game.getSquareValue(second) + 
+            " " + game.getSquareValue(third) 
+        )
+    }
+    
+    def gameOver {
+        println(game.getStatus)
+    }
     
     def getInput: String = {
         return readLine.trim
--- src/test/scala/ConsoleUISpec.scala
+++ src/test/scala/ConsoleUISpec.scala
@@ -1,34 +1,58 @@
 import org.scalatest.Spec
 import org.scalatest.BeforeAndAfter
 import org.mockito.Mockito._
+import org.mockito.Matchers._
 import java.io.PrintStream
 import java.io.BufferedReader
 
 import com.tictactoe.ConsoleUI
-
+import com.tictactoe.Game
 
 class ConsoleUISpec extends Spec with BeforeAndAfter {
+    var mockOut = createMockOut
+    var mockIn = createMockIn
+    var mockGame = createMockGame
     
-    var ui = new ConsoleUI()
-    var mockOut = mock(classOf[PrintStream])
-    var mockIn = mock(classOf[BufferedReader])
+    var ui = new ConsoleUI(mockGame)
+
+    
+    def createMockOut = mock(classOf[PrintStream])
+    def createMockIn = mock(classOf[BufferedReader])
+    def createMockGame = mock(classOf[Game])
     
     before {
-        ui = new ConsoleUI()
-        mockOut = mock(classOf[PrintStream])
-        mockIn = mock(classOf[BufferedReader])
+        mockGame = createMockGame
+        ui = new ConsoleUI(mockGame)
+        
+        mockOut = createMockOut
         ui.io.setOut(mockOut)
+        
+        mockIn = createMockIn
         ui.in = mockIn
     }
     
-    describe("prints to user") {
-        var ui = new ConsoleUI()
-        var mockOut = mock(classOf[PrintStream])
-        ui.io.setOut(mockOut)
+    describe("output") {
         
-        ui.welcomeUser
+        it("displays a message") {
+            ui.displayMessage("Welcome To Tic Tac Toe in Scala")
+
+            verify(mockOut).println("Welcome To Tic Tac Toe in Scala")
+        }
         
-        verify(mockOut).println("Welcome to Tic Tac Toe in Scala")
+        it("displays square info from game") {
+            ui.displayBoard
+            
+            verify(mockGame, times(9)).getSquareValue(anyInt())
+        }
+        
+        it("game over message") {
+            when(mockGame.getStatus).thenReturn("X wins!")
+            
+            ui.gameOver
+            
+            verify(mockOut).println("X wins!")
+        }
+        
     }
     
     describe("input") {
@@ -37,6 +61,8 @@ class ConsoleUISpec extends Spec with BeforeAndAfter {
             
             expect("foo") { ui.getInput }
         }
+        
     }
+    
     
 }

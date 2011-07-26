18fafb0572b9bb64701f69346c065e1f91f112a8
Mike Jansen <mjansen401@gmail.com>
2011-07-26 11:02:14 -0500

    update project configuration

--- project/build/TicTacToe.scala
+++ project/build/TicTacToe.scala
@@ -1,6 +1,8 @@
 import sbt._
 
 class TicTacToe(info: ProjectInfo) extends DefaultProject(info) {
+
+    // Dependencies
     val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1" % "test"
     val mockito = "org.mockito" % "mockito-all" % "1.8.5" % "test"
 }

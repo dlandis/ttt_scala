499400c3caa95ef83ce4829222407207f3b8e5d6
Mike Jansen <mjansen401@gmail.com>
2011-07-26 16:04:56 -0500

    clean up consoleUI tests

--- src/test/scala/ConsoleUISpec.scala
+++ src/test/scala/ConsoleUISpec.scala
@@ -84,14 +84,13 @@ class ConsoleUISpec extends Spec with BeforeAndAfter {
         
         it("prompts user until valid input received")  {
             var mockParser = mock(classOf[StringParser])
-            
             when (mockParser.isValid(anyString()))
                 .thenReturn(false)
                 .thenReturn(false)
                 .thenReturn(true)    
-            when(mockIn.readLine).thenReturn("foo")
-            
-            val validate = (input:String) => mockParser.isValid(input)    
+            when(mockIn.readLine).thenReturn("foo") 
+            val validate = (input:String) => mockParser.isValid(input) 
+               
             expect("foo") { ui.getValidInput(validate, "enter foo") }  
             
             verify(mockOut, times(3)).printf("enter foo")          
@@ -101,7 +100,6 @@ class ConsoleUISpec extends Spec with BeforeAndAfter {
         it("gets a move from user") {
             var mockParser = mock(classOf[SquareParser])
             ui.squareParser = mockParser
-            
             when (mockParser.isValid(anyString())).thenReturn(true)
             when(mockIn.readLine).thenReturn("3")
             when(mockParser.parsedInput("3")).thenReturn(2)
@@ -114,7 +112,6 @@ class ConsoleUISpec extends Spec with BeforeAndAfter {
         it("asks user to play again") {
             var mockParser = mock(classOf[PlayAgainParser])
             ui.playAgainParser = mockParser
-            
             when (mockParser.isValid(anyString())).thenReturn(true)
             when(mockIn.readLine).thenReturn("foo")
             when(mockParser.parsedInput("foo")).thenReturn(false)

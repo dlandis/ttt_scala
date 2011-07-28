5978a179117584ee4dc91be55e3f70452d61f7e8
Mike Jansen <mjansen401@gmail.com>
2011-07-28 16:28:50 -0500

    add onejar plugin

--- project/build/TicTacToe.scala
+++ project/build/TicTacToe.scala
@@ -1,6 +1,7 @@
 import sbt._
+import com.github.retronym.OneJarProject
 
-class TicTacToe(info: ProjectInfo) extends DefaultProject(info) {
+class TicTacToe(info: ProjectInfo) extends DefaultProject(info) with OneJarProject {
 
     // Dependencies
     val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1" % "test"
